92619bef611ec105b986f8452b4b09fc59815d0c
jaballadares <jballadares28@gmail.com>
2015-08-27 17:04:25 -0700

    i have no idea of 80 percent of  what Im doing

--- library/js/styleguide.js
+++ library/js/styleguide.js
@@ -1,28 +1,43 @@
-var copyEmailBtn = document.querySelector('.copy');
-copyEmailBtn.addEventListener('click', function(event) {
-  // Select the email link anchor text
-  var emailLink = document.querySelector('.green-text');
+// var copyEmailBtn = document.querySelector('.copy');
+// copyEmailBtn.addEventListener('click', function(event) {
+//   // Select the email link anchor text
+//   var emailLink = document.querySelector('.green-text');
+//   var range = document.createRange();
+//   range.selectNode(emailLink);
+//   window.getSelection().addRange(range);
+//
+//   try {
+//     // Now that we've selected the anchor text, execute the copy command
+//     var successful = document.execCommand('copy');
+//     var msg = successful ? 'successful' : 'unsuccessful';
+//     console.log('Copy email command was ' + msg);
+//   } catch (err) {
+//     console.log('Oops, unable to copy');
+//   }
+//
+//   // Remove the selections - NOTE: Should use
+//   // removeRange(range) when it is supported
+//   window.getSelection().removeAllRanges();
+// });
+
+var copyBtn = document.querySelector('.hover-notification');
+copyBtn.addEventListener('click', function() {
+  var urlField = document.querySelector('.hex-code');
+
+  // create a Range object
   var range = document.createRange();
-  range.selectNode(emailLink);
+  // set the Node to select the "range"
+  range.selectNode(urlField);
+  // add the Range to the set of window selections
   window.getSelection().addRange(range);
 
-  try {
-    // Now that we've selected the anchor text, execute the copy command
-    var successful = document.execCommand('copy');
-    var msg = successful ? 'successful' : 'unsuccessful';
-    console.log('Copy email command was ' + msg);
-  } catch (err) {
-    console.log('Oops, unable to copy');
-  }
+  // execute 'copy', can't 'cut' in this case
+  document.execCommand('copy');
+}, false);
 
-  // Remove the selections - NOTE: Should use
-  // removeRange(range) when it is supported
-  window.getSelection().removeAllRanges();
-});
-
-function changeNotification() {
-  var x = $(this).children($('.hover-notification'));
-  x.html('Copied!');
-}
-
-$('.front').click(changeNotification);
+// function changeNotification() {
+//   var x = $(this).children($('.hover-notification'));
+//   x.html('Copied!');
+// }
+//
+// $('.front').click(changeNotification);
