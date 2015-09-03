6d14c91b29537d1500a3e2ba5d06008b8a9e964f
jaballadares <jballadares28@gmail.com>
2015-09-03 15:35:20 -0700

    making some progress

--- library/js/styleguide.js
+++ library/js/styleguide.js
@@ -1,43 +1,72 @@
-// var copyEmailBtn = document.querySelector('.copy');
-// copyEmailBtn.addEventListener('click', function(event) {
-//   // Select the email link anchor text
-//   var emailLink = document.querySelector('.green-text');
+// var copyBtn = document.querySelector('.hover-notification');
+// copyBtn.addEventListener('click', function() {
+//
+//   var urlField = document.querySelectorAll('.hex-code');
+//
+//   // create a Range object
 //   var range = document.createRange();
-//   range.selectNode(emailLink);
+//   // set the Node to select the "range"
+//   range.selectNode(urlField);
+//   // add the Range to the set of window selections
 //   window.getSelection().addRange(range);
 //
-//   try {
-//     // Now that we've selected the anchor text, execute the copy command
-//     var successful = document.execCommand('copy');
-//     var msg = successful ? 'successful' : 'unsuccessful';
-//     console.log('Copy email command was ' + msg);
-//   } catch (err) {
-//     console.log('Oops, unable to copy');
-//   }
+//   // execute 'copy', can't 'cut' in this case
+//   document.execCommand('copy');
+// }, false);
 //
-//   // Remove the selections - NOTE: Should use
-//   // removeRange(range) when it is supported
-//   window.getSelection().removeAllRanges();
+// $('.hover-notification').click(function() {
+//   $(this)
 // });
-
-var copyBtn = document.querySelector('.hover-notification');
-copyBtn.addEventListener('click', function() {
-  var urlField = document.querySelector('.hex-code');
-
-  // create a Range object
-  var range = document.createRange();
-  // set the Node to select the "range"
-  range.selectNode(urlField);
-  // add the Range to the set of window selections
-  window.getSelection().addRange(range);
-
-  // execute 'copy', can't 'cut' in this case
-  document.execCommand('copy');
-}, false);
-
 // function changeNotification() {
 //   var x = $(this).children($('.hover-notification'));
 //   x.html('Copied!');
 // }
 //
 // $('.front').click(changeNotification);
+
+
+var copyButton = $('.hover-notification');
+copyButton.on('click', function(event) {
+  var $this = $(this);
+
+  var content = $this.closest('.swatch-container').find('.hex-code');
+
+  var range = document.createRange();
+  range.selectNode(content[0]);
+
+  window.getSelection().addRange(range);
+
+  try {
+    var successful = document.execCommand('copy');
+    if (successful) {
+      $(this).after('<span class="success">Copied!</span>');
+      setTimeout(function() {
+        $('.success').addClass('show');
+        setTimeout(function() {
+          $('.success').fadeOut(function() {
+            $('.success').remove();
+          });
+        }, 1000);
+      }, 0);
+    }
+
+  } catch (err) {}
+  window.getSelection().removeAllRanges();
+});
+// var copyBtn = document.querySelector('.hover-notification');
+
+// var copyBtn = document.querySelector('.swatch-container');
+// copyBtn.addEventListener('click', function(e) {
+//
+//   var urlField = e.currentTarget.parentElement.parentElement.querySelector('.hex-code');
+//
+//   // create a Range object
+//   var range = document.createRange();
+//   // set the Node to select the "range"
+//   range.selectNode(urlField);
+//   // add the Range to the set of window selections
+//   window.getSelection().addRange(range);
+//
+//   // execute 'copy', can't 'cut' in this case
+//   document.execCommand('copy');
+// }, false);
